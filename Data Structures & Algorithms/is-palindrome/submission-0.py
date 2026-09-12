2a5bb506727fbd547d522d32833368f4db12a048
Anant Gupta <89693238+Procurio-AG@users.noreply.github.com>
2026-09-12 22:10:18 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        temp=s
+        s=[]
+        for i in temp:
+            if i.isalnum():
+                s.append(i.lower())
+        l,r=0,len(s)-1
+        while(l<r):
+            if s[l]!=s[r]:
+                return False
+            l+=1
+            r-=1
+
+        return True
