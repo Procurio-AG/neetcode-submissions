05c547a92b7aadb05cefab14c538f82d1d71cd89
Anant Gupta <89693238+Procurio-AG@users.noreply.github.com>
2026-09-14 01:17:08 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        def correspond(sym: str) -> str:
+            match sym:
+                case ')':
+                    return '('
+                case ']':
+                    return '['
+                case '}':
+                    return '{'
+                case _:
+                    return None
+        
+        stack=[]
+        for sym in s:
+            if stack:
+                if correspond(sym)==stack[-1]:
+                    stack.pop()
+                else:
+                    stack.append(sym)
+            else:
+                stack.append(sym)
+        if not(stack):
+            return True
+        return False
+        
